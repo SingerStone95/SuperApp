94bcefb04e90cff8dc83f13ed598b19dd9dff0fd
yogachen <445191096@qq.com>
2018-11-12 20:21:58 +0800

    test commit

--- app.me/src/main/java/singerstone/com/appme/MainActivity.java
+++ app.me/src/main/java/singerstone/com/appme/MainActivity.java
@@ -2,6 +2,7 @@ package singerstone.com.appme;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import singerstone.com.libbase.Test;
 
@@ -12,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Test.test();
+        testmerge();
     }
+
+    private void testmerge() {
+        Log.i("123", "123");
+    }
+
 }
