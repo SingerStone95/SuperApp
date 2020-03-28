a04035a0827daf9568d4c7eef5d32a519224c060
yogachen <yogachen@tencent.com>
2020-03-29 01:57:18 +0800

    挪动目录

--- LeetCode/src/test/java/com/singerstone/java/VarietyStoreTest.java
+++ LeetCode/src/test/java/com/singerstone/java/VarietyStoreTest.java
@@ -1,15 +1,16 @@
 package com.singerstone.java;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class VarietyStoreTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void updateItems() {
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         VarietyStore app = new VarietyStore(items);
         app.updateItems();
         assertEquals("fixme", app.items[0].name);
     }
-
 }
