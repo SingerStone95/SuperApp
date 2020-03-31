7654336d42d2f8a4295a1e0daed169c1f4e43240
yogachen <445191096@qq.com>
2020-03-31 09:56:21 +0800

    update fucking code 注释

---
+++ LeetCode/src/test/java/com/singerstone/java/base/AbsSellPriceStrategyTest.java
@@ -0,0 +1,28 @@
+package com.singerstone.java.base;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+
+import static org.junit.Assert.*;
+
+/**
+ * author : yogachen
+ * date   : 2020-03-31
+ * desc   :
+ */
+public class AbsSellPriceStrategyTest {
+
+    @Test
+    public void update() throws Exception {
+        AbsSellPriceStrategy absSellPriceStrategy = PowerMockito.mock(AbsSellPriceStrategy.class);
+        Item item = new Item("茅台", 2, 0);
+        PowerMockito.doCallRealMethod().when(absSellPriceStrategy, "update", item);
+        PowerMockito.doReturn(1).when(absSellPriceStrategy, "getPriceChangeCount", Mockito.anyInt());
+        PowerMockito.doReturn(false).when(absSellPriceStrategy, "needLimitPrice");
+        PowerMockito.doReturn(-1).when(absSellPriceStrategy, "getChangeSellDeadline");
+        absSellPriceStrategy.update(item);
+        Assert.assertEquals(1, item.price);
+    }
+}
