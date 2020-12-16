76d8c349b9a9ca0ebc201fb1f17e713fb0995fa7
yogachen <yogachen@tencent.com>
2020-12-16 22:01:14 +0800

    测试字节码插桩

---
+++ LeetCode/src/main/java/com/singerstone/bytecode/ByteCodeTest.java
@@ -0,0 +1,19 @@
+package com.singerstone.bytecode;
+
+import java.io.File;
+
+public class ByteCodeTest {
+
+    public static void main(String[] args) {
+        String rootPath = System.getProperty("user.dir");
+        String classFilePath =
+                rootPath + File.separator + "LeetCode" + File.separator + "Test.java";
+        System.out.println("yogachen->" + classFilePath);
+        File classFile = new File(classFilePath);
+        if (classFile.exists()){
+            System.out.println("yogachen->" + "has file");
+        }
+
+    }
+
+}
