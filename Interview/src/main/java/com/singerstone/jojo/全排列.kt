67b06c70bd35e63fa006a9a46a7f5e13efc8b2b7
yogachen <yogachen@tencent.com>
2022-06-24 00:02:49 +0800

    删除垃圾

--- Interview/src/main/java/com/singerstone/jojo/全排列.kt
+++ Interview/src/main/java/com/singerstone/jojo/全排列.kt
@@ -4,6 +4,32 @@ class 全排列 {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
+            val array = arrayOf(1, 2, 3, 4)
+            val result = arrayListOf<ArrayList<Int>>()
+            val visit = ArrayList<Boolean>(array.size)
+            for (i in array){
+                visit.add(false)
+
+            }
+            permutations(array, result, visit, arrayListOf<Int>())
+            print(result)
+
+        }
+
+
+        private fun permutations(array: Array<Int>, result: ArrayList<ArrayList<Int>>, visit: ArrayList<Boolean>, tmp: ArrayList<Int>) {
+            if (tmp.size == array.size) {
+                result.add(ArrayList(tmp))
+                return
+            }
+            for (i in array.indices) {
+                if (visit[i]) continue
+                visit[i] = true
+                tmp.add(array[i])
+                permutations(array, result, visit, ArrayList(tmp))
+                visit[i] = false
+                tmp.removeLast()
+            }
 
         }
 
---
+++ Interview/src/main/java/com/singerstone/jojo/收集雨水.kt
@@ -0,0 +1,55 @@
+package com.singerstone.jojo
+
+import kotlin.math.min
+
+/**
+ * 有两种解法 这里实现最优的第二种
+ * 单指针 两次遍历,第一次从左往右找到每个节点的最大值，第二次从右到左找到每个节点的最大值，取两者最小值，减去当前节点的值
+ * 双指针 一次遍历
+ * 42题
+ */
+class 收集雨水 {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+//            val water = arrayOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)
+            val water = arrayOf(0, 1, 0, 2,3)
+
+
+            print(collectWater(water))
+
+        }
+
+        private fun collectWater(water: Array<Int>): Int {
+            var left = 0
+            var right = water.size - 1
+            var res = 0
+            while (left < right) {
+                val mini = min(water[left], water[right])
+                //小的那一边先开始
+                if (mini == water[left]) {
+                    left++
+                    while (left < right && water[left] < mini) {
+                        res += (mini - water[left])
+                        left++
+
+                    }
+
+                } else {
+                    right--
+                    while (left < right && water[right] < mini) {
+                        res += (mini - water[right])
+                        right--
+                    }
+
+                }
+            }
+
+            return res
+
+        }
+    }
+
+}
