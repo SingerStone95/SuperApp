878b0e8d977d660db4e741d55c5238d7bad1e4c8
yogachen <yogachen@tencent.com>
2022-06-27 23:48:43 +0800

    删除垃圾

--- Interview/src/main/java/com/singerstone/jojo/IP地址.kt
+++ Interview/src/main/java/com/singerstone/jojo/IP地址.kt
@@ -4,7 +4,6 @@ class IP地址 {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-
             val input = "25525511135"
             val result = mutableListOf<String>()
             val tmp = ""
@@ -13,12 +12,13 @@ class IP地址 {
         }
 
         private fun ipConfig(input: String, result: MutableList<String>, seg: Int, tmp: String) {
-            if (seg == 0) {
-                if (tmp.isEmpty()) {
+            //seg : 表示进入了第 4-seg 段 ，0 开始计数
+            if (seg == 0) { // 因为没有第4段 所以递归在这里结束
+                if (input.isEmpty()) { // 恰好把数都用完
                     result.add(tmp)
                 }
             } else {
-                //最多3位数
+                //每一段最多3位数
                 for (i in 1..3) {
                     if (input.length >= i && isValid(input.substring(0, i))) {
                         ipConfig(input.substring(i), result, seg - 1, if (seg == 1) "$tmp${input.substring(0, i)}" else "$tmp${input.substring(0, i)}.")
