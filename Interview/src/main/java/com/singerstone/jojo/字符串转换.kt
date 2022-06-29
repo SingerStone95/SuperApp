bfa179711b84b7e072d99f99f7db728ef30c5415
yogachen <yogachen@tencent.com>
2022-06-29 10:25:34 +0800

    Merge branch 'master' of https://github.com/SingerStone95/SuperApp
    # Conflicts:
    #	Interview/src/main/java/com/singerstone/jojo/全排列.kt
    #	Interview/src/main/java/com/singerstone/jojo/链表.kt
    #	Interview/src/main/java/com/singerstone/jojo/链表翻转123.kt
    #	Interview/src/main/java/com/singerstone/test/链表.kt

--- Interview/src/main/java/com/singerstone/jojo/字符串转换.kt
+++ Interview/src/main/java/com/singerstone/jojo/字符串转换.kt
@@ -4,7 +4,51 @@ class 字符串转换 {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
+            val input = "    -2149asds"
+            println(atoi(input))
+        }
 
+        fun atoi(input: String): Int {
+            var pi = 0
+            while (input[pi] == ' ') {
+                pi++
+            }
+            var flag = 0 // 0是正数
+            if (input[pi] == '+' || input[pi] == '-') {
+                if (input[pi] == '-') {
+                    flag = 1
+                }
+                pi++
+            }
+            var result = 0
+            while (pi < input.length && input[pi] in '0'..'9') {
+                if (result > Int.MAX_VALUE / 10) {
+                    return if (flag == 1) {
+                        Int.MAX_VALUE
+                    } else {
+                        Int.MIN_VALUE
+                    }
+                }
+                var cur = input[pi] - '0'
+                if (result == Int.MAX_VALUE / 10) {
+                    if (flag == 1 && cur > 8) {
+                        return Int.MIN_VALUE
+                    }
+                    if (flag == 0 && cur > 7) {
+                        return Int.MAX_VALUE
+                    }
+                }
+                result = result * 10 + cur
+                pi++
+            }
+
+            return result * if (flag == 0) 1 else -1
+        }
+
+        // 正则解法
+        fun atoi2(input: String): Int {
+
+            return 0
         }
     }
 }
