80d7cad1006c5af2dfd57869acdde6d90d314670
yogachen <yogachen@tencent.com>
2022-06-30 11:23:47 +0800

    update

--- Interview/src/main/java/com/singerstone/jojo/验证括号.kt
+++ Interview/src/main/java/com/singerstone/jojo/验证括号.kt
@@ -4,7 +4,32 @@ class 验证括号 {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
+            println(isValid("[{]}"))
+        }
 
+        fun isValid(input: String): Boolean {
+            val stack = mutableListOf<Char>()
+            val map = mapOf(
+                '[' to ']',
+                '(' to ')',
+                '{' to '}'
+            )
+            for (c in input) {
+                if (c == '[' || c == '{' || c == '(') {
+                    stack.add(c)
+                } else if (c == ']' || c == '}' || c == ')') {
+                    if (stack.size == 0) {
+                        return false
+                    }
+                    if (map[stack.removeLast()] != c) {
+                        return false
+                    }
+                } else {
+                    return false
+                }
+            }
+
+            return true
         }
     }
 }
