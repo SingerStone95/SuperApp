15e4e257214541866ca82aac68bcbbae588a1fd7
yogachen <yogachen@tencent.com>
2022-07-05 23:35:38 +0800

    update

---
+++ Interview/src/main/java/com/singerstone/jojo/括号问题.kt
@@ -0,0 +1,10 @@
+package com.singerstone.jojo
+
+class 括号问题 {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+        }
+    }
+}
---
+++ Interview/src/main/java/com/singerstone/jojo/路径问题.kt
@@ -0,0 +1,29 @@
+package com.singerstone.jojo
+
+class 路径问题 {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            print(uniquePath(7, 3))
+        }
+
+        // leetcode 62
+        // 怎么把二维数组转化为一维数组
+        fun uniquePath(m: Int, n: Int): Int {
+            var dp = Array(m) { IntArray(n) }
+            //dp[i][j]=dp[i-1][j]+dp[i][j-1]
+            for (i in 0 until m) {
+                for (j in 0 until n) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1
+                    } else {
+                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+                    }
+                }
+            }
+            return dp[m - 1][n - 1]
+
+        }
+    }
+}
