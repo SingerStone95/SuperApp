ce53ac94f1ccf409a733e076ef8898df860fd481
yogachen <yogachen@tencent.com>
2022-07-25 21:51:10 +0800

    update windows环境

--- Interview/src/main/java/com/singerstone/jojo/字符串子序列问题.kt
+++ Interview/src/main/java/com/singerstone/jojo/字符串子序列问题.kt
@@ -1,16 +1,55 @@
 package com.singerstone.jojo
 
+import kotlin.math.max
+
 class 字符串子序列问题 {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-
-            print(longestSubStr("a"))
+            println(longestSubStr("a"))
+            println(longestConSubStr(arrayOf(100, 4, 200, 1, 3, 2, 5, 6)))
 
         }
 
+        /**
+         * 最长连续子序列 （序列可以不连续）
+         */
+        fun longestConSubStr(input: Array<Int>): Int {
+            val set = hashSetOf<Int>()
+            set.addAll(input)
+            var result = 1
+            for (i in input.indices) {
+                if (!set.contains(input[i])) {
+                    continue
+                }
+                set.remove(input[i])
+                var left = i - 1
+                var right = i + 1
+                while (left >= 0) {
+                    if (set.contains(left)) {
+                        set.remove(input[left])
+                        left--
+                    } else {
+                        break
+                    }
+                }
+                while (right < input.size) {
+                    if (set.contains(right)) {
+                        set.remove(input[right])
+                        right++
+                    } else {
+                        break
+                    }
+                }
+                result = max(result, right - left - 1)
 
-        //aabcacbd
+            }
+            return result
+        }
+
+        /**
+         *   aabcacbd 最长不重复子串 子串必须要连续
+         */
         fun longestSubStr(source: String): String {
             if (source.isNullOrEmpty()) {
                 return source
