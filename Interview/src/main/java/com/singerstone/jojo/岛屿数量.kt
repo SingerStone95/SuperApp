c79b95bbb787c133d50301a83899b97edf38a18e
yogachen <yogachen@tencent.com>
2022-08-16 00:03:56 +0800

    update

--- Interview/src/main/java/com/singerstone/jojo/岛屿数量.kt
+++ Interview/src/main/java/com/singerstone/jojo/岛屿数量.kt
@@ -1,5 +1,7 @@
 package com.singerstone.jojo
 
+import java.util.*
+
 class 岛屿数量 {
 
     companion object {
@@ -11,18 +13,65 @@ class 岛屿数量 {
              * 1, 1, 0, 0, 0
              * 0, 0, 0, 0, 1
              */
-            val input = arrayOf(intArrayOf(1, 1, 1, 1, 0), intArrayOf(1, 1, 0, 1, 0), intArrayOf(1, 1, 0, 0, 0), intArrayOf(0, 0, 0, 0, 1))
-            println(numOfIslands(input))
+            val input = arrayOf(intArrayOf(1, 1, 1, 1, 0), intArrayOf(1, 1, 0, 1, 0), intArrayOf(1, 1, 0, 0, 0), intArrayOf(0, 0, 0, 0, 0))
+//            println(numOfIslands(input))
+            println(numOfIslandsB(input))
         }
 
         /**
-         * todo
          * bfs
          */
         private fun numOfIslandsB(input: Array<IntArray>): Int {
+            val visited = arrayOf(intArrayOf(0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 0, 0))
+            var result = 0
+            for (i in input.indices) {
+                for (j in input[i].indices) {
+                    if (visited[i][j] == 1 || input[i][j] == 0) {
+                        continue
+                    }
+                    result++
+                    bfs(input, visited, i, j)
+                }
+            }
+            return result
+        }
 
-            return 0
+        private fun bfs(input: Array<IntArray>, visited: Array<IntArray>, i: Int, j: Int) {
+            val queue = LinkedList<Pair<Int, Int>>()
+            queue.add(Pair(i, j))
+            while (queue.isNotEmpty()) {
+                val pair = queue.removeFirst()
+                if (input[pair.first][pair.second] == 1) {
+                    visited[pair.first][pair.second] = 1
+                    if (isValid(pair.first - 1, pair.second, input, visited)) {
+                        queue.addLast(Pair(pair.first - 1, pair.second))
+                    }
+                    if (isValid(pair.first + 1, pair.second, input, visited)) {
+                        queue.addLast(Pair(pair.first + 1, pair.second))
+                    }
+                    if (isValid(pair.first, pair.second + 1, input, visited)) {
+                        queue.addLast(Pair(pair.first, pair.second + 1))
+                    }
+                    if (isValid(pair.first, pair.second - 1, input, visited)) {
+                        queue.addLast(Pair(pair.first, pair.second - 1))
+                    }
+                }
 
+            }
+
+        }
+
+        private fun isValid(i: Int, j: Int, input: Array<IntArray>, visited: Array<IntArray>): Boolean {
+            if (i < 0 || i >= input[0].size || j >= input.size || j < 0) {
+                return false
+            }
+            if (input[i][j] == 0) {
+                return false
+            }
+            if (visited[i][j] == 1) {
+                return false
+            }
+            return true
         }
 
         /**
@@ -37,7 +86,7 @@ class 岛屿数量 {
                         continue
                     }
                     result++
-                    visitIsland(input, visited, i, j)
+                    dfs(input, visited, i, j)
                 }
             }
             return result
@@ -47,16 +96,16 @@ class 岛屿数量 {
         /**
          * dfs
          */
-        private fun visitIsland(input: Array<IntArray>, visited: Array<IntArray>, i: Int, j: Int) {
+        private fun dfs(input: Array<IntArray>, visited: Array<IntArray>, i: Int, j: Int) {
             if (i < 0 || i >= input.size || j < 0 || j >= input.size
                     || visited[i][j] == 1 || input[i][j] == 0) {
                 return
             }
             visited[i][j] = 1
-            visitIsland(input, visited, i - 1, j)
-            visitIsland(input, visited, i, j - 1)
-            visitIsland(input, visited, i, j + 1)
-            visitIsland(input, visited, i + 1, j)
+            dfs(input, visited, i - 1, j)
+            dfs(input, visited, i, j - 1)
+            dfs(input, visited, i, j + 1)
+            dfs(input, visited, i + 1, j)
 
 
         }
--- Interview/src/main/java/com/singerstone/jojo/滑动窗口.kt
+++ Interview/src/main/java/com/singerstone/jojo/滑动窗口.kt
@@ -4,11 +4,16 @@ class 滑动窗口 {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
+            var input = arrayOf<Int>()
+            println(maxSlideWindow(input, 2))
 
         }
 
-        fun maxSlideWindow() {
-// todo
+        fun maxSlideWindow(input: Array<Int>, k: Int): Array<Int> {
+            var result = arrayListOf<Int>()
+
+            return result.toArray(arrayOf())
+
         }
     }
 
