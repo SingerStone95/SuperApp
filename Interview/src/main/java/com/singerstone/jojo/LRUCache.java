954d5ce2d37996020933208b977014b749f43ba7
yogachen <yogachen@tencent.com>
2022-09-12 12:26:31 +0800

    update

--- Interview/src/main/java/com/singerstone/jojo/LRUCache.java
+++ Interview/src/main/java/com/singerstone/jojo/LRUCache.java
@@ -3,8 +3,6 @@ package com.singerstone.jojo;
 public class LRUCache {
 
     public static void main(String[] args) {
-
-        // todo
     }
 
 }
--- Interview/src/main/java/com/singerstone/jojo/岛屿数量.kt
+++ Interview/src/main/java/com/singerstone/jojo/岛屿数量.kt
@@ -1,6 +1,7 @@
 package com.singerstone.jojo
 
 import java.util.*
+import kotlin.collections.ArrayList
 
 class 岛屿数量 {
 
@@ -16,6 +17,13 @@ class 岛屿数量 {
             val input = arrayOf(intArrayOf(1, 1, 1, 1, 0), intArrayOf(1, 1, 0, 1, 0), intArrayOf(1, 1, 0, 0, 0), intArrayOf(0, 0, 0, 0, 0))
 //            println(numOfIslands(input))
             println(numOfIslandsB(input))
+
+            /**
+             * Input: m = 3, n = 3, positions = [[0,0], [0,1], [1,2], [2,1]]
+             * Output: [1,1,2,3]
+             */
+            val input2 = arrayOf<IntArray>(intArrayOf(0, 0), intArrayOf(0, 1), intArrayOf(1, 2), intArrayOf(2, 1))
+            print(island2(3, 3, input2))
         }
 
         /**
@@ -108,6 +116,67 @@ class 岛屿数量 {
             dfs(input, visited, i + 1, j)
 
 
+        }
+
+        /**
+         * Input: m = 3, n = 3, positions = [[0,0], [0,1], [1,2], [2,1]]
+         * Output: [1,1,2,3]
+         */
+        private fun island2(m: Int, n: Int, input: Array<IntArray>): ArrayList<Int> {
+            val result = arrayListOf<Int>()
+            var num = 0
+            val island = Array<Int>(m * n) {
+                -1
+            }
+            for (i in input) {
+                //算出在island的坐标
+                val index = i[0] * n + i[1]
+                if (island[index] == -1) {
+                    island[index] = index
+                    num++
+                }
+                // 依次遍历他的邻居 1,2,3,4 左上右下
+                for (direct in 1..4) {
+                    val neighbor = getNeighbor(direct, i[0], i[1])
+                    if (neighbor[0] < 0 || neighbor[0] >= m
+                            || neighbor[1] < 0 || neighbor[1] >= n
+                            || island[neighbor[0] * n + neighbor[1]] == -1) {
+                        continue
+                    }
+                    // 获取邻居的根节点
+                    val q = getRoot(i[0] * n + i[1], island)
+                    val p = getRoot(neighbor[0] * n + neighbor[1], island)
+                    if (p != q) {
+                        // 让邻居节点的根节点指向当前节点，可以理解成一个链表，在链表上的元素都是一个集合，这个是比较巧妙的
+                        island[p] = q
+                        num--
+                    }
+                }
+                result.add(num)
+            }
+            return result
+        }
+
+        private fun getRoot(index: Int, island: Array<Int>): Int {
+            return if (island[index] != index) {
+                getRoot(island[index], island)
+            } else {
+                index
+            }
+        }
+
+
+        //1,2,3,4 左上右下
+        private fun getNeighbor(i: Int, i1: Int, i2: Int): IntArray {
+            return if (i == 1) {
+                intArrayOf(i1 - 1, i2)
+            } else if (i == 2) {
+                intArrayOf(i1, i2 - 1)
+            } else if (i == 3) {
+                intArrayOf(i1 + 1, i2)
+            } else {
+                intArrayOf(i1, i2 + 1)
+            }
         }
     }
 }
