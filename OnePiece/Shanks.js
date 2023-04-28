05b7b71cd5906553bebce9661b162c565ff76274
yogachen <yogachen@tencent.com>
2023-04-28 14:13:01 +0800

    [feat] demo

--- OnePiece/Shanks.js
+++ OnePiece/Shanks.js
@@ -1,1 +1,21 @@
-console.log('start');
+import { ChatGPTAPI } from 'chatgpt'
+import fs from 'fs';
+
+async function main() {
+
+  var data = fs.readFileSync('article.txt');
+  var text = data.toString("utf8");
+  text = text.replace(/\s*/g, "");
+  text = text.replace('图片', "");
+  text = text.replace(/[`:_.~!@#$%^&*() \+ =<>?"{}|, \/ ;' \\ [ \] ·~！@#￥%……&*（）—— \+ ={}|《》？：“”【】、；‘’，。、]/g, '');
+  const api = new ChatGPTAPI({
+    apiKey: ''
+  })
+
+
+  console.log(text.length);
+  const res = await api.sendMessage(text);
+  console.log(res.text);
+}
+
+main();
