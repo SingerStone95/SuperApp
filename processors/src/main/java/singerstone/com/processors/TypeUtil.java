098da5962db2aa719a4d0914fa3dc5f18c6f0b36
yogachen <yogachen@tencent.com>
2023-08-07 19:46:14 +0800

    删除无用代码

--- processors/src/main/java/singerstone/com/processors/TypeUtil.java
+++ processors/src/main/java/singerstone/com/processors/TypeUtil.java
@@ -6,6 +6,5 @@ public class TypeUtil {
     public static final ClassName ANDROID_VIEW = ClassName.get("android.view", "View");
     public static final ClassName ANDROID_ON_CLICK_LISTENER = ClassName.get("android.view", "View", "OnClickListener");
     public static final ClassName INJET = ClassName.get("singerstone.com.inject", "Inject");
-    public static final ClassName INJETTest = ClassName.get("singerstone.com.inject", "InjectTest");
     public static final ClassName PROVIDER = ClassName.get("singerstone.com.inject.provider","Provider");
 }
