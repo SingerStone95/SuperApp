d13656c5e61fec05fdd6b86c82a470ad7ff29a6b
yogachen <yogachen@tencent.com>
2023-08-07 14:56:31 +0800

    更新一些代码

--- Interview/src/main/java/com/singerstone/jojo/二叉树的镜像.kt
+++ Interview/src/main/java/com/singerstone/jojo/二叉树的镜像.kt
@@ -1,4 +1,27 @@
 package com.singerstone.jojo
 
+import com.singerstone.jojo.二叉树构建.Companion.visitTree
+
 class 二叉树的镜像 {
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val array = arrayOf(0, 1, 2, 3, 4, 5, 6)
+            val root = 二叉树构建.makeTreeRecursion(array, 0, array.size)
+            visitTree(mirrorTree(root))
+        }
+
+        fun mirrorTree(root: TreeNode?): TreeNode? {
+            if (root == null) {
+                return null
+            }
+            val left = mirrorTree(root.left)
+            val right = mirrorTree(root.right)
+            root.left = right
+            root.right = left
+            return root
+        }
+
+    }
 }
--- Interview/src/main/java/com/singerstone/jojo/回文问题.kt
+++ Interview/src/main/java/com/singerstone/jojo/回文问题.kt
@@ -7,6 +7,8 @@ class 回文问题 {
             val input = "abccbcccc"
             println(palindromic2(input))
 
+            println(isPalindromicNum(10011))
+
         }
 
         // 经典回文子串非 dp 解法
@@ -52,18 +54,18 @@ class 回文问题 {
             for (i in source.length - 1 downTo 0) {
                 for (j in i until source.length) {
                     //123456
-                        //66
-                            //55 56
-                                //44 45 46
-                                    //33 34 35 36
-                                        // 22 23 24 25 26
-                                            // 11 12 13 14 15 16
-                                                // 00 01 02 03 04 05 06
+                    //66
+                    //55 56
+                    //44 45 46
+                    //33 34 35 36
+                    // 22 23 24 25 26
+                    // 11 12 13 14 15 16
+                    // 00 01 02 03 04 05 06
                     if (i == j) {
                         array[i][j] = 1
                         continue
                     }
-                    if (source[i] == source[j] && (j - i  <= 2 || array[i + 1][j - 1] == 1)) {
+                    if (source[i] == source[j] && (j - i <= 2 || array[i + 1][j - 1] == 1)) {
                         array[i][j] = 1
                         if (j - i + 1 > max) {
                             max = j - i + 1
@@ -80,9 +82,13 @@ class 回文问题 {
 
         // 回文数字
         fun isPalindromicNum(num: Int): Boolean {
-
-
-            return false
+            var num_ = num
+            var revers = 0
+            while (num_ != 0) {
+                revers = (revers * 10 + num_ % 10)
+                num_ /= 10
+            }
+            return revers == num
         }
     }
 }
