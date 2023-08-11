fb8ce3408f2905c198a66dd5e96a35d3a7b312b3
yogachen <yogachen@tencent.com>
2023-08-11 17:36:09 +0800

    update

--- Interview/src/main/java/com/singerstone/jojo/二叉搜索树与双向链表.java
+++ Interview/src/main/java/com/singerstone/jojo/二叉搜索树与双向链表.java
@@ -1,7 +1,37 @@
 package com.singerstone.jojo;
 
-public class 二叉搜索树与双向链表 {
-    public Node treeToDoublyList(Node root) {
+import static com.singerstone.jojo.二叉搜索树.buildSearchTree;
 
+public class 二叉搜索树与双向链表 {
+    public static void main(String[] args) {
+        Integer[] array = {1, 2, 3, 4, 5, 6};
+        buildSearchTree(array, 0, array.length - 1);
+
+    }
+
+    // 中序遍历
+    public TreeNode treeToDoublyList(TreeNode root) {
+        dfs(root);
+        head.left = pre;
+        pre.right = head;
+        return head;
+    }
+
+    TreeNode pre = null;
+    TreeNode head = null;
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        if (pre == null) {
+            head = root;
+        } else {
+            root.left = pre;
+            pre.right = root;
+        }
+        pre = root;
+        dfs(root.right);
     }
 }
--- Interview/src/main/java/com/singerstone/jojo/二叉搜索树的后序遍历.java
+++ Interview/src/main/java/com/singerstone/jojo/二叉搜索树的后序遍历.java
@@ -1,7 +1,11 @@
 package com.singerstone.jojo;
 
 public class 二叉搜索树的后序遍历 {
-    public boolean verifyPostorder(int[] postorder) {
+    public static void main(String[] args) {
 
+    }
+
+    public boolean verifyPostorder(int[] postorder) {
+        return false;
     }
 }
