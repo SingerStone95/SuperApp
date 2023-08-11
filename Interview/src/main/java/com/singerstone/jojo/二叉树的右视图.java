67326c5b0d3fc044839ccae6109f5f018d2c3ad8
yogachen <yogachen@tencent.com>
2023-08-11 16:16:50 +0800

    update

--- Interview/src/main/java/com/singerstone/jojo/二叉树的右视图.java
+++ Interview/src/main/java/com/singerstone/jojo/二叉树的右视图.java
@@ -1,9 +1,45 @@
 package com.singerstone.jojo;
 
+import static com.singerstone.jojo.二叉树构建.makeTreeRecursion;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class 二叉树的右视图 {
-    public List<Integer> rightSideView(TreeNode root) {
+
+    public static void main(String[] args) {
+        Integer[] values = {0, 1, 2, 3, 4};
+        TreeNode root = makeTreeRecursion(values, 0, values.length);
+        System.out.println(rightSideView(root));
+    }
+
+
+    // 两种思路第一种思路直接按dfs把信息存map<行号，节点>
+    // 第二种方式不使用递归
+    public static List<Integer> rightSideView(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        List<Integer> result = new ArrayList<>();
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.removeFirst();
+                if (node.left != null) {
+                    queue.addLast(node.left);
+                }
+                if (node.right != null) {
+                    queue.addLast(node.right);
+                }
+                if (size - 1 == i) {
+                    result.add(node.value);
+                }
+            }
+        }
+        return result;
 
     }
 }
--- Interview/src/main/java/com/singerstone/jojo/序列化二叉树.java
+++ Interview/src/main/java/com/singerstone/jojo/序列化二叉树.java
@@ -1,14 +1,67 @@
 package com.singerstone.jojo;
 
+import static com.singerstone.jojo.二叉树构建.makeTreeRecursion;
+import static com.singerstone.jojo.二叉树构建.visitTree;
+
+import java.util.ArrayList;
+
 public class 序列化二叉树 {
+
+    public static void main(String[] args) {
+        Integer[] values = {0, 1, 2, 3, 4};
+        TreeNode root = makeTreeRecursion(values, 0, values.length);
+        System.out.println();
+        String serialize = new 序列化二叉树().serialize(root);
+        System.out.println(serialize);
+
+        TreeNode node=new 序列化二叉树().deserialize(serialize);
+        visitTree(node);
+    }
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-
+        dfs(root);
+        return result.substring(0, result.length() - 1);
     }
+
+    String result = "";
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            result += "null,";
+            return;
+        }
+        result += root.value;
+        result += ",";
+        dfs(root.left);
+        dfs(root.right);
+    }
+
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
+        if (data.isEmpty()) {
+            return null;
+        }
+        String[] list = data.split(",");
+        ArrayList<String> arr = new ArrayList();
+        for (String node : list) {
+            arr.add(node);
+        }
+
+        return dfs2(arr);
+    }
+
+    TreeNode dfs2(ArrayList<String> arr) {
+        if (arr.get(0).equals("null")) {
+            arr.remove(0);
+            return null;
+        }
+        TreeNode root = new TreeNode(Integer.valueOf(arr.get(0)));
+        arr.remove(0);
+        root.left = dfs2(arr);
+        root.right = dfs2(arr);
+        return root;
 
     }
 
