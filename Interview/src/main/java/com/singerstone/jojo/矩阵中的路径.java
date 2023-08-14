725499564f49aa186d8dcec4358f5ff961163b52
yogachen <yogachen@tencent.com>
2023-08-14 20:38:54 +0800

    update

--- Interview/src/main/java/com/singerstone/jojo/矩阵中的路径.java
+++ Interview/src/main/java/com/singerstone/jojo/矩阵中的路径.java
@@ -6,6 +6,35 @@ public class 矩阵中的路径 {
     }
 
     public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (exist(board, word, 0, 0, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+
+    public boolean exist(char[][] board, String word, int i, int j, int k) {
+        if (i < 0 || i >= board[0].length || j < 0 || j >= board.length || k >= word.length()) {
+            return false;
+        }
+        if (board[i][j] != word.charAt(k)) {
+            return false;
+        }
+        if (k == word.length() - 1) {
+            return true;
+        }
+        board[i][j] = '\0';
+        boolean exist = exist(board, word, i + 1, j, k + 1) ||
+                exist(board, word, i - 1, j, k + 1) ||
+                exist(board, word, i, j + 1, k + 1) ||
+                exist(board, word, i, j - 1, k + 1);
+        board[i][j] = word.charAt(k);
+        return exist;
+
 
     }
 }
