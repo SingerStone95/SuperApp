1804231de2341e5128903704d8fda31653389401
yogachen <yogachen@tencent.com>
2023-08-15 16:28:07 +0800

    update

--- Interview/src/main/java/com/singerstone/jojo/复原IP地址.java
+++ Interview/src/main/java/com/singerstone/jojo/复原IP地址.java
@@ -1,10 +1,53 @@
 package com.singerstone.jojo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class 复原IP地址 {
+    public static void main(String[] args) {
+        System.out.println(new 复原IP地址().restoreIpAddresses("19216811"));
+    }
+
     public List<String> restoreIpAddresses(String s) {
-        return null;
+        List<String> result = new ArrayList<>();
+        restoreIpAddresses(s, 4, "", result);
+        return result;
+
+    }
+
+    public void restoreIpAddresses(String s, int seg, String temp, List<String> result) {
+
+        if (s.isEmpty() && seg > 0) {
+            return;
+        } else if (seg == 0) {
+            if (s.isEmpty()) {
+                result.add(temp);
+            } else {
+                return;
+            }
+        } else {
+            for (int i = 1; i <= 3 && i <= s.length(); i++) {
+                String segment = s.substring(0, i);
+                if (isValidSeg(segment, seg)) {
+                    String next = temp + segment;
+                    if (seg > 1) {
+                        next += '.';
+                    }
+                    restoreIpAddresses(s.substring(i), seg - 1, next, result);
+                }
+            }
+        }
+    }
+
+    private boolean isValidSeg(String s, int seg) {
+        if (s.isEmpty()) {
+            return false;
+        }
+        int ip = Integer.valueOf(s);
+        if (seg == 4 && ip == 0) {
+            return false;
+        }
+        return ip >= 0 && ip <= 255;
 
     }
 }
--- Interview/src/main/java/com/singerstone/jojo/括号生成.java
+++ Interview/src/main/java/com/singerstone/jojo/括号生成.java
@@ -1,12 +1,34 @@
 package com.singerstone.jojo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class 括号生成 {
 
+    public static void main(String[] args) {
+        System.out.println(new 括号生成().generateParenthesis(3));
+
+    }
 
     public List<String> generateParenthesis(int n) {
-        return null;
+        List<String> result = new ArrayList<>();
+        generateParenthesis(n, n, result, "");
+        return result;
 
+    }
+
+    public void generateParenthesis(int left, int right, List<String> result, String tmp) {
+        if (left == 0 && right == 0) {
+            result.add(tmp);
+        }
+        if (left > right) {
+            return;
+        }
+        if (left > 0) {
+            generateParenthesis(left - 1, right, result, tmp + "(");
+        }
+        if (right > 0) {
+            generateParenthesis(left, right - 1, result, tmp + ")");
+        }
     }
 }
