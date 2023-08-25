2b623b7751f54a92957ec88aa5ca7c1e086c65fb
yogachen <yogachen@tencent.com>
2023-08-25 12:20:51 +0800

    update

--- Interview/src/main/java/com/singerstone/jojo/栈的压入弹出序列.java
+++ Interview/src/main/java/com/singerstone/jojo/栈的压入弹出序列.java
@@ -1,11 +1,50 @@
 package com.singerstone.jojo;
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class 栈的压入弹出序列 {
     public static void main(String[] args) {
+/*        LinkedList<Integer> stack = new LinkedList<>();
+        stack.push(129);
+        int[] popped = new int[]{10, 11, 129};
+        for (int pop : popped) {
+            System.out.println(stack.contains(pop));
+        }*/
+        //pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+        int[] pushed = new int[]{1, 2, 3, 4, 5};
+        int[] popped = new int[]{4,3,5,1,2};
+        System.out.println(new 栈的压入弹出序列().validateStackSequences(pushed, popped));
 
     }
 
+    //输入：pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        return false;
+        LinkedList<Integer> stack = new LinkedList<>();
+        int start = 0;
+        for (int pop : popped) {
+            if (!stack.contains(pop)) {
+                while (start < pushed.length) {
+                    stack.addLast(pushed[start]);
+                    if (pushed[start] == pop) {
+                        start++;
+                        break;
+                    }
+                    start++;
+                }
+            }
+            while (!stack.isEmpty()) {
+                int top = stack.getLast();
+                if (top == pop) {
+                    stack.removeLast();
+                    break;
+                } else {
+                    return false;
+                }
+            }
+
+        }
+        return true;
     }
+
 }
