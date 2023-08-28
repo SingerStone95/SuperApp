b8776ccabb58445a75a299b55fc6783c49aeced1
yogachen <yogachen@tencent.com>
2023-08-28 17:14:36 +0800

    update

--- Interview/src/main/java/com/singerstone/jojo/合并区间.java
+++ Interview/src/main/java/com/singerstone/jojo/合并区间.java
@@ -1,10 +1,59 @@
 package com.singerstone.jojo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class 合并区间 {
+    public static void main(String[] args) {
 
+        int[][] array = new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        printArray(new 合并区间().merge(array));
+
+
+    }
+
+    public static void printArray(int[][] intervals) {
+        for (int[] array : intervals) {
+            for (int num : array) {
+                System.out.print(num);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+     * 输出：[[1,6],[8,10],[15,18]]
+     * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+     */
     public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+        ArrayList<int[]> result = new ArrayList<>();
+        int[] temp = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            int[] array = intervals[i];
+            if (array[0] <= temp[1]) { //可以连续
+                temp[1] = Math.max(array[1], temp[1]);
+            } else {
+                result.add(temp);
+                temp = intervals[i];
+            }
+            if (i==intervals.length-1){
+                result.add(temp);
+            }
+        }
 
-        return new int[][]{};
+        return result.toArray(new int[0][]); // 返回二维数组的惯用写法
     }
 
 }
---
+++ Interview/src/main/java/com/singerstone/jojo/和为K的子数组个数.java
@@ -0,0 +1,32 @@
+package com.singerstone.jojo;
+
+import java.util.HashMap;
+
+public class 和为K的子数组个数 {
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 1, 1};
+        System.out.println(new 和为K的子数组个数().subarraySum(array, 2));
+
+    }
+
+
+    // 前提：一定是连续子数组，负责规则不适用
+    // 用 pre[i] 表示从 0-i 的和
+    // 在数组中 存在 pre[j]-pre[i-1] =k (其实就是 num 区间 i - j)
+    // 所以该方法转化为在 pre 组书中寻找有 x 组差为 k 的组合
+    public int subarraySum(int[] nums, int k) {
+        int result = 0;
+        int pre = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); // 如果 pre == k 直接结果加1
+        for (int num : nums) {
+            pre += num;
+            Integer other = map.get(pre - k);
+            if (other != null) {
+                result += other;
+            }
+            map.put(pre, map.getOrDefault(pre, 0) + 1);
+        }
+        return result;
+    }
+}
