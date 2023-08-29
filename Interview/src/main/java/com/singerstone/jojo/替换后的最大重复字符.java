48bd40d1b247188ec36b7de1551c52746183908d
yogachen <yogachen@tencent.com>
2023-08-29 21:26:58 +0800

    update

--- Interview/src/main/java/com/singerstone/jojo/替换后的最大重复字符.java
+++ Interview/src/main/java/com/singerstone/jojo/替换后的最大重复字符.java
@@ -1,7 +1,15 @@
 package com.singerstone.jojo;
 
 public class 替换后的最大重复字符 {
-    public int characterReplacement(String s, int k) {
+    public static void main(String[] args) {
 
+    }
+
+    public int characterReplacement(String s, int k) {
+        int start = 0;
+        for (int end = 0; end < s.length(); end++) {
+
+        }
+        return 0;
     }
 }
--- Interview/src/main/java/com/singerstone/jojo/滑动窗口的最大值.java
+++ Interview/src/main/java/com/singerstone/jojo/滑动窗口的最大值.java
@@ -1,5 +1,42 @@
 package com.singerstone.jojo;
 
-public class 滑动窗口的最大值 {
 
+import static com.singerstone.jojo.二叉树的遍历.printArray;
+
+import java.util.LinkedList;
+
+public class 滑动窗口的最大值 {
+    public static void main(String[] args) {
+        int[] array = new int[]{4,3,11};
+        printArray(new 滑动窗口的最大值().maxSlidingWindow(array, 3));
+
+    }
+
+    // nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] result = new int[nums.length - k + 1];
+        int m = 0;
+        LinkedList<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < k; i++) {
+            // 一直添加到队列里面比当前这个数大的后面
+            while (!queue.isEmpty() && queue.getLast() < nums[i]) {
+                queue.removeLast();
+            }
+            queue.addLast(nums[i]);
+        }
+        result[m++] = queue.getFirst();
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i - k] == queue.getFirst()) {
+                queue.removeFirst();
+            }
+            while (!queue.isEmpty() && queue.getLast() < nums[i]) {
+                queue.removeLast();
+            }
+            queue.addLast(nums[i]);
+            result[m++] = queue.getFirst();
+        }
+
+        return result;
+
+    }
 }
