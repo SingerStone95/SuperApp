f1843d76c8150fcbbdf24d9d7d028d848a5d615e
yogachen <yogachen@tencent.com>
2023-08-31 22:13:10 +0800

    update

--- Interview/src/main/java/com/singerstone/jojo/复杂链表的复制.java
+++ Interview/src/main/java/com/singerstone/jojo/复杂链表的复制.java
@@ -1,6 +1,10 @@
 package com.singerstone.jojo;
 
 public class 复杂链表的复制 {
+
+    public static void main(String[] args) {
+
+    }
     public Node copyRandomList(Node head) {
 
         return null;
