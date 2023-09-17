6722a23a86041ca135bf7afab9b6c7238280b0f1
yogachen <yogachen@tencent.com>
2023-09-17 20:18:49 +0800

    update

--- Interview/src/main/java/com/singerstone/jojo/字母异位词分组.java
+++ Interview/src/main/java/com/singerstone/jojo/字母异位词分组.java
@@ -1,5 +1,7 @@
 package com.singerstone.jojo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,12 +14,25 @@ import java.util.Map;
  */
 public class 字母异位词分组 {
 
+    public static void main(String[] args) {
+        String[] array=new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(new 字母异位词分组().groupAnagrams(array));
+    }
+
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
         for (String s : strs) {
-
+            char[] array = s.toCharArray();
+            Arrays.sort(array);
+            String key = String.valueOf(array);
+            List<String> value = map.get(key);
+            if (value == null) {
+                value = new ArrayList<>();
+            }
+            value.add(s);
+            map.put(key, value);
         }
 
-        return null;
+        return new ArrayList<>(map.values());
     }
 }
