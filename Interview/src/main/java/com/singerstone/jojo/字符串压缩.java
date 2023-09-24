55754df6853ff3513ff94447230a172af6009669
yogachen <yogachen@tencent.com>
2023-09-24 15:37:27 +0800

    update

--- Interview/src/main/java/com/singerstone/jojo/字符串压缩.java
+++ Interview/src/main/java/com/singerstone/jojo/字符串压缩.java
@@ -3,8 +3,9 @@ package com.singerstone.jojo;
 public class 字符串压缩 {
 
     public static void main(String[] args) {
-        System.out.println(new 字符串压缩().compressString("a"));
+        System.out.println(new 字符串压缩().compressString("aaa"));
     }
+
     //示例1:
     //
     // 输入："aabcccccaaa"
@@ -13,22 +14,22 @@ public class 字符串压缩 {
         if (S.isEmpty()) {
             return "";
         }
-        String result = "";
+        StringBuilder result = new StringBuilder();
         char last_char = S.charAt(0);
         int count = 1;
         for (int i = 1; i < S.length(); i++) {
             char c = S.charAt(i);
             if (last_char != c) {
-                result += last_char;
-                result += count;
+                result.append(last_char);
+                result.append(count);
                 count = 0;
             }
             last_char = c;
             count++;
         }
-        result += last_char;
-        result += count;
-        return S.length()>result.length()?result:S;
+        result.append(last_char);
+        result.append(count);
+        return S.length() > result.length() ? result.toString() : S;
 
     }
 }
--- Interview/src/main/java/com/singerstone/jojo/整数转罗马数字.java
+++ Interview/src/main/java/com/singerstone/jojo/整数转罗马数字.java
@@ -1,7 +1,31 @@
 package com.singerstone.jojo;
 
 public class 整数转罗马数字 {
+
+    public static void main(String[] args) {
+        System.out.println(new 整数转罗马数字().intToRoman(3344));
+    }
+
+    /**
+     * 直接暴力枚举
+     * 字符          数值
+     * I             1
+     * V             5
+     * X             10
+     * L             50
+     * C             100
+     * D             500
+     * M             1000
+     * 3333
+     */
     public String intToRoman(int num) {
-return "";
+        String[] array1 = new String[]{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+        String[] array2 = new String[]{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"};
+        String[] array3 = new String[]{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", "M"};
+        String[] array4 = new String[]{"", "M", "MM", "MMM"};
+        return array4[num / 1000]
+                + array3[num % 1000 / 100]
+                + array2[num % 100 / 10]
+                + array1[num % 10];
     }
 }
