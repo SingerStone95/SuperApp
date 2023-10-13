e0a65c76f937eb5b722a3ee8b111339938d4dc3b
yogachen <yogachen@tencent.com>
2023-10-13 17:46:24 +0800

    update

--- Interview/src/main/java/com/singerstone/jojo/反转字符串中的单词.java
+++ Interview/src/main/java/com/singerstone/jojo/反转字符串中的单词.java
@@ -2,27 +2,47 @@ package com.singerstone.jojo;
 
 public class 反转字符串中的单词 {
     public static void main(String[] args) {
+        System.out.println(new 反转字符串中的单词().reverseWords("a good   example"));
 
     }
 
     /**
      * 示例 1：
-     *
+     * <p>
      * 输入：s = "the sky is blue"
      * 输出："blue is sky the"
      * 示例 2：
-     *
+     * <p>
      * 输入：s = "  hello world  "
      * 输出："world hello"
      * 解释：反转后的字符串中不能存在前导空格和尾随空格。
      * 示例 3：
-     *
+     * <p>
      * 输入：s = "a good   example"
      * 输出："example good a"
      * 解释：如果两个单词间有多余的空格，反转后的字符串需要将单词间的空格减少到仅有一个。
      */
     public String reverseWords(String s) {
-        return "";
+        String[] items = s.trim().split(" ");
+        StringBuilder builder = new StringBuilder();
+        for (int i = items.length - 1; i >= 0; i--) {
+            String tmp = items[i].trim();
+            if (tmp.isEmpty()) {
+                continue;
+            }
+            builder.append(tmp);
+            if (i != 0) {
+                builder.append(" ");
+            }
+
+        }
+        return builder.toString();
+
+    }
+
+    public String reverseWords2(String s) {
+
+
 
     }
 }
