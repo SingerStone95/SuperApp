2c68a007a3b0d09dee31f6eaa76c163041421ec6
yogachen <yogachen@tencent.com>
2023-10-13 12:32:35 +0800

    update

---
+++ Interview/src/main/java/com/singerstone/jojo/反转字符串中的单词.java
@@ -0,0 +1,28 @@
+package com.singerstone.jojo;
+
+public class 反转字符串中的单词 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 示例 1：
+     *
+     * 输入：s = "the sky is blue"
+     * 输出："blue is sky the"
+     * 示例 2：
+     *
+     * 输入：s = "  hello world  "
+     * 输出："world hello"
+     * 解释：反转后的字符串中不能存在前导空格和尾随空格。
+     * 示例 3：
+     *
+     * 输入：s = "a good   example"
+     * 输出："example good a"
+     * 解释：如果两个单词间有多余的空格，反转后的字符串需要将单词间的空格减少到仅有一个。
+     */
+    public String reverseWords(String s) {
+        return "";
+
+    }
+}
---
+++ Interview/src/main/java/com/singerstone/jojo/颜色分类.java
@@ -0,0 +1,16 @@
+package com.singerstone.jojo;
+
+public class 颜色分类 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 输入：nums = [2,0,2,1,1,0]
+     * 输出：[0,0,1,1,2,2]
+     */
+    public void sortColors(int[] nums) {
+
+    }
+}
