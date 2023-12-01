e424eea9850adba30dd895bc9b6c7d4390593541
yogachen <yogachen@tencent.com>
2023-12-01 17:32:25 +0800

    update

---
+++ Interview/src/main/java/com/singerstone/jojo/projects/生产者消费者模型BlockingQueue.java
@@ -0,0 +1,64 @@
+package com.singerstone.jojo.projects;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class 生产者消费者模型BlockingQueue {
+
+    private static Integer count = 0;
+    private static final int FULL = 10;
+    //创建一个阻塞队列
+    private final BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue<>(FULL);
+
+    public static void main(String[] args) {
+        生产者消费者模型BlockingQueue test3 = new 生产者消费者模型BlockingQueue();
+        new Thread(test3.new Producer()).start();
+        new Thread(test3.new Consumer()).start();
+    }
+
+    class Producer implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                try {
+                    blockingQueue.put(1);
+                    count++;
+                    System.out.println(Thread.currentThread().getName()
+                            + "生产者生产，目前总共有" + count);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class Consumer implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+                try {
+                    blockingQueue.take();
+                    count--;
+                    System.out.println(Thread.currentThread().getName()
+                            + "消费者消费，目前总共有" + count);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
