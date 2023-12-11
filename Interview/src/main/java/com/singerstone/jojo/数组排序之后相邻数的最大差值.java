d3fee58d7cf045f5b27f573f8506e5269cb3c761
yogachen <yogachen@tencent.com>
2023-12-11 11:44:56 +0800

    update

--- Interview/src/main/java/com/singerstone/jojo/数组排序之后相邻数的最大差值.java
+++ Interview/src/main/java/com/singerstone/jojo/数组排序之后相邻数的最大差值.java
@@ -2,12 +2,63 @@ package com.singerstone.jojo;
 
 public class 数组排序之后相邻数的最大差值 {
     public static void main(String[] args) {
+        int[] nums = new int[]{494767408,862126209,213511142,768240025,631263193,839199222,990848796,214568815,540853864,760724418,980162605,976743981,168965760,680875496,256709197,970953816,948680062,347254784,732201399,786249847,782805044,40906641,674241381,784330934,175502610,731105392,424650824,549764101,986090032,710542549,249208107,448419816,527708664,158499246,223421723,114552457,466978424,821491411,19614107,115389497,902211438,2644108,744489871,897895073,372311214,551142753,933294894,426217662,217504874,983488406,516890023,426853110,971124103};
+        System.out.println(new 数组排序之后相邻数的最大差值().maxGap(nums));
 
     }
 
-    public static int maxGap(int[] nums) {
+    public int maxGap(int[] nums) {
+        if (nums.length<=1) {
+            return 0;
+        }
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[i]);
+        }
+        if (max == min) {
+            return 0;
+        }
+        int len = nums.length;
+        boolean[] bucketHasValue = new boolean[len + 1];
+        int[] bucketMax = new int[len + 1];
+        int[] bucketMin = new int[len + 1];
+        for (int i = 0; i < nums.length; i++) {
+            int index = calculateBucketIndex(max, min, nums[i], len);
+            if (bucketHasValue[index]) {
+                bucketMax[index] = Math.max(bucketMax[index], nums[i]);
+                bucketMin[index] = Math.min(bucketMin[index], nums[i]);
+            } else {
+                bucketMax[index] = nums[i];
+                bucketMin[index] = nums[i];
+                bucketHasValue[index] = true;
+            }
+        }
+        int i = 0;
+        for (; i <= len; i++) {
+            if (bucketHasValue[i]) {
+                break;
+            }
+        }
+        int preMax = bucketMax[i];
+        i++;
+        int result = 0;
+        for (; i <= len; i++) {
+            if (bucketHasValue[i]) {
+                result = Math.max(result, bucketMin[i] - preMax);
+                preMax = bucketMax[i];
+            }
+        }
 
+        return result;
 
     }
+
+    private int calculateBucketIndex(long max, long min, long num, long len) {
+        //(num-min)/((max-min)/len)
+        return (int) ((num - min) * len / (max - min));
+    }
+
 
 }
