03a1d0b6f38f115b390e405e364cd13680194fc4
yogachen <yogachen@tencent.com>
2024-01-16 12:54:35 +0800

    update

--- Interview/src/main/java/com/singerstone/jojo/二叉树最大路径和.java
+++ Interview/src/main/java/com/singerstone/jojo/二叉树最大路径和.java
@@ -7,18 +7,26 @@ public class 二叉树最大路径和 {
 
 
     public static void main(String[] args) {
-        Integer[] values = {0, 1, 2, 3, 4};
+        Integer[] values = {-10, 1, 2, 3, 4};
         TreeNode root = makeTreeRecursion(values, 0, values.length);
         System.out.println(new 二叉树最大路径和().maxPathSum(root));
     }
 
+    int max = Integer.MIN_VALUE;
 
     public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return max;
+
+    }
+
+    public int dfs(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        int left = maxPathSum(root.left);
-        int right = maxPathSum(root.right);
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        max = Math.max(max, Math.max(left, 0) + Math.max(right, 0) + root.value);
         //返回当前节点贡献值 ,必须是包含了当前节点
         return root.value + Math.max(Math.max(left, 0), Math.max(right, 0));
     }
