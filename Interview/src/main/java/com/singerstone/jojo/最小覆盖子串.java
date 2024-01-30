7a7b32d8b0e733c338e43c162c324675c7dc46aa
yogachen <yogachen@tencent.com>
2024-01-30 17:15:53 +0800

    新增题目

---
+++ Interview/src/main/java/com/singerstone/jojo/最小覆盖子串.java
@@ -0,0 +1,30 @@
+package com.singerstone.jojo;
+
+public class 最小覆盖子串 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 示例 1：
+     * <p>
+     * 输入：s = "ADOBECODEBANC", t = "ABC"
+     * 输出："BANC"
+     * 解释：最小覆盖子串 "BANC" 包含来自字符串 t 的 'A'、'B' 和 'C'。
+     * 示例 2：
+     * <p>
+     * 输入：s = "a", t = "a"
+     * 输出："a"
+     * 解释：整个字符串 s 是最小覆盖子串。
+     * 示例 3:
+     * <p>
+     * 输入: s = "a", t = "aa"
+     * 输出: ""
+     * 解释: t 中两个字符 'a' 均应包含在 s 的子串中，
+     * 因此没有符合条件的子字符串，返回空字符串。
+     */
+    public String minWindow(String s, String t) {
+        return "";
+
+    }
+}
---
+++ Interview/src/main/java/com/singerstone/jojo/用Rand7实现Rand10.java
@@ -0,0 +1,12 @@
+package com.singerstone.jojo;
+
+public class 用Rand7实现Rand10 {
+    public static void main(String[] args) {
+
+    }
+
+    public int rand10() {
+        return 0;
+
+    }
+}
