ee5d1629b572fceb5624fb85c904506588385bf6
yogachen <yogachen@tencent.com>
2024-04-10 20:25:00 +0800

    更新题库

--- Interview/src/main/java/com/singerstone/jojo/tencent2024/客户端第一题.java
+++ Interview/src/main/java/com/singerstone/jojo/tencent2024/客户端第一题.java
@@ -1,12 +1,38 @@
 package com.singerstone.jojo.tencent2024;
 
+import com.singerstone.jojo.ListNode;
+
+import static com.singerstone.jojo.NodeUtilKt.printLinkList;
+import static com.singerstone.jojo.排序链表2023.printListNode;
+
 public class 客户端第一题 {
 
     /**
      * 删除链表中值为k的数
      */
     public static void main(String[] args) {
+        ListNode head = new ListNode(2, new ListNode(3,
+                new ListNode(2, new ListNode(1, null))));
+        printListNode(deleteNode(head, 2));
 
-        //ListNode* deleteNode(ListNode* head, int k) {
+    }
+
+    // 1 -> 2 -> 3
+    public static ListNode deleteNode(ListNode head, int k) {
+        ListNode dummy = new ListNode(-1, head);
+        ListNode pre = dummy;
+
+        while (head != null) {
+            if (head.value == k) {
+                pre.next = head.next;
+            } else {
+                pre = head;
+            }
+            head = head.next;
+        }
+
+        return dummy.next;
+
+
     }
 }
--- Interview/src/main/java/com/singerstone/jojo/tencent2024/客户端第二题.java
+++ Interview/src/main/java/com/singerstone/jojo/tencent2024/客户端第二题.java
@@ -1,14 +1,43 @@
 package com.singerstone.jojo.tencent2024;
 
+import com.singerstone.jojo.TreeNode;
+
+import static com.singerstone.jojo.二叉树构建.makeTreeRecursion;
+
 public class 客户端第二题 {
     /**
      * 给定二叉树，每个点价值是0或1，问从根节点到叶子节点组成的二进制数有多少个在l,r范围中
      * 遍历往下走，大于r返回，最后看是不是大于等于l
+     *
      * @param args
      */
     public static void main(String[] args) {
+        Integer[] array = {1, 1, 1, 1, 1, 0, 0};
+        TreeNode root = makeTreeRecursion(array, 0, array.length);
+        System.out.println(number_of_different_plans(root,7,7));
+    }
 
-        //int number_of_different_plans(TreeNode* root, int l, int r)
+    static int result = 0;
+
+    public static int number_of_different_plans(TreeNode root, int l, int r) {
+        dfs(root, l, r, 0);
+        return result;
+    }
+
+    public static void dfs(TreeNode root, int l, int r, int sum) {
+        if (root == null) {
+            return;
+        }
+        int cur = (sum << 1) + root.value;
+        if (cur > r) {
+            return;
+        }
+        if (root.left == null && root.right == null && cur >= l && cur <= r) {
+            result++;
+            return;
+        }
+        dfs(root.left, l, r, cur);
+        dfs(root.right, l, r, cur);
     }
 
 }
