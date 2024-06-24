30809689f13b62bc39629e5406aeff87f1c88d8f
yogachen <yogachen@tencent.com>
2024-06-24 23:43:05 +0800

    update

---
+++ Interview/src/main/java/com/singerstone/jojo/H指数.java
@@ -0,0 +1,20 @@
+package com.singerstone.jojo;
+
+public class H指数 {
+    /**
+     * 示例 1：
+     *
+     * 输入：citations = [3,0,6,1,5]
+     * 输出：3
+     * 解释：给定数组表示研究者总共有 5 篇论文，每篇论文相应的被引用了 3, 0, 6, 1, 5 次。
+     *      由于研究者有 3 篇论文每篇 至少 被引用了 3 次，其余两篇论文每篇被引用 不多于 3 次，所以她的 h 指数是 3。
+     * 示例 2：
+     *
+     * 输入：citations = [1,3,1]
+     * 输出：1
+     */
+
+    public int hIndex(int[] citations) {
+
+    }
+}
---
+++ Interview/src/main/java/com/singerstone/jojo/文本左右对齐.java
@@ -0,0 +1,49 @@
+package com.singerstone.jojo;
+
+import java.util.List;
+
+public class 文本左右对齐 {
+
+    /**
+     * 示例 1:
+     *
+     * 输入: words = ["This", "is", "an", "example", "of", "text", "justification."], maxWidth = 16
+     * 输出:
+     * [
+     *    "This    is    an",
+     *    "example  of text",
+     *    "justification.  "
+     * ]
+     * 示例 2:
+     *
+     * 输入:words = ["What","must","be","acknowledgment","shall","be"], maxWidth = 16
+     * 输出:
+     * [
+     *   "What   must   be",
+     *   "acknowledgment  ",
+     *   "shall be        "
+     * ]
+     * 解释: 注意最后一行的格式应为 "shall be    " 而不是 "shall     be",
+     *      因为最后一行应为左对齐，而不是左右两端对齐。
+     *      第二行同样为左对齐，这是因为这行只包含一个单词。
+     * 示例 3:
+     *
+     * 输入:words = ["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"]，maxWidth = 20
+     * 输出:
+     * [
+     *   "Science  is  what we",
+     *   "understand      well",
+     *   "enough to explain to",
+     *   "a  computer.  Art is",
+     *   "everything  else  we",
+     *   "do                  "
+     * ]
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public List<String> fullJustify(String[] words, int maxWidth) {
+
+    }
+}
